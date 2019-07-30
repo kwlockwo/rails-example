935687f2178270a67a10ae28d8ee992de95625dc
Keith Lockwood <kwlockwo@gmail.com>
2019-07-30 13:44:10 +1000

    get chrome working

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -81,14 +81,15 @@ RSpec.configure do |config|
   require 'capybara/rspec'
   require "selenium/webdriver"
 
+  chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+  chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+
   Capybara.register_driver :chrome do |app|
     Capybara::Selenium::Driver.new(app, browser: :chrome)
   end
 
   Capybara.register_driver :headless_chrome do |app|
-    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-      chromeOptions: { args: %w(headless disable-gpu) }
-    )
+    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
 
     Capybara::Selenium::Driver.new app,
       browser: :chrome,
